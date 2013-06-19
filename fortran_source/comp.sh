b13092014fbb892d2002a89d0997f1cc6958aeb0
Lukasz Mentel <lmmentel@gmail.com>
2013-06-19 03:41:32 +0200

    moved the gamess class to a separate module in the chemjob package olny the dmft speficic stuff is left. the two electron integral tools (factor and ijkl) are move to module integrals in chemjob. fortran sources for reading the files are used now in the gamesspraser class from the gamessus module in the chemjob package.

--- fortran_source/comp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-f2py -m onelectron  --fcompiler=gfortran -c onelectron.f90
-f2py -m twoelectron --fcompiler=gfortran -c twoelectron.f90
-
-cp onelectron.so ..
-cp twoelectron.so ..
--- pydmft.py
+++ pydmft.py
@@ -1,135 +1,13 @@
 #!/usr/bin/env python
 
 import argparse
+import gamessus 
+from integrals import factor, ijkl
 import numpy as np
 import matplotlib.pyplot as mplot 
 import os
 import re
 import sys
-
-# fortran interfaces fof reading files 
-import onelectron
-import twoelectron
-
-class Gamess(object):
-    '''Simple class for holding gamess filenames for a given job.'''
-    def __init__(self, filename=""):
-        if filename:
-            self.filebase   = os.path.splitext(filename)[0] 
-            self.inputfile  = self.filebase + ".inp"
-            self.outputfile = self.filebase + ".log"
-            self.twoeaofile = self.filebase + ".F08"
-            self.twoemofile = self.filebase + ".F09"
-            self.dictionary = self.filebase + ".F10"
-            self.rdm2file   = self.filebase + ".F15"
-            self.aos        = self.get_number_of_aos()
-            self.mos        = self.get_number_of_mos()
-        else:
-            sys.exit('Gamess file not specified, exiting...') 
-
-    def get_number_of_aos(self):
-        '''Get the number of primitive cartesian gaussian basis functions from 
-           Gamess log file'''
-
-        p = r'NUMBER OF CARTESIAN GAUSSIAN BASIS FUNCTIONS =\s*(\d+)'
-        cp= re.compile(p)
-
-        with open(self.outputfile, 'r') as f:
-            lines = f.readlines()
-
-        for line in lines:
-            match = cp.search(line)
-            if match:
-                return int(match.group(1))
-
-    def get_number_of_mos(self):
-        '''Get the number of molecular orbitals from Gammess log file.'''
-
-#   check if the calculation is done in spherical of cartesian gaussians
-#   base on the value of ISPHER keyword
-        ispher_patt    = r'ISPHER=\s*(\-?\d{1}).*'
-        var_space_patt = r'.*VARIATION SPACE IS\s*(\d+).*'
-        c_ispher = re.compile(ispher_patt)
-        c_var_space = re.compile(var_space_patt)
-
-        with open(self.outputfile, 'r') as f:
-            lines = f.readlines()
-
-        for line in lines:
-            match = c_ispher.search(line)
-            if match:
-                ispher = int(match.group(1)) 
-
-        n_ao = self.get_number_of_aos()
-        if ispher == -1: 
-            n_mo = n_ao
-            return n_mo
-        elif ispher == 1:
-            for line in lines:
-                match = c_var_space.search(line)
-                if match:
-                    n_mo = int(match.group(1))
-                    return n_mo
-        else:
-            sys.exit("ispher not found") 
-
-
-    def get_onee_size(self):
-        '''Get the size of the vector holding upper (or lower) triangle
-           of a square matrix of size nmos.'''
-        n = self.mos
-        return n*(n+1)/2
-
-    def get_twoe_size(self):
-        '''Get the size of the 1d vector holding upper (or lower) triangle
-           of a supermatrix of size nmos (2RDM and two-electrons integrals) .'''
-        n = self.get_onee_size()
-        return n*(n+1)/2
-
-    def get_rdm2(self):
-        '''Read the 2rdm from the gamess-us file'''
-# initialize numpy array to zeros
-        rdm2 = np.zeros(self.get_twoe_size(), dtype=float)
-# use gamess module to read the integrals from the file -filename-
-        if os.path.exists(self.rdm2file):
-            twoelectron.integrals.readinao(rdm2, self.rdm2file)
-            return rdm2
-        else: 
-            sys.exit("File '{0:s}' doesn't exist, exiting...".format(self.rdm2file))
-
-    def get_motwoe(self):
-        '''Read the tow electron integrals from the gamess-us file'''
-# initialize numpy array to zeros
-        twoe = np.zeros(self.get_twoe_size(), dtype=float)
-# use gamess module to read the integrals from the file -filename-
-        if os.path.exists(self.twoemofile):
-            twoelectron.integrals.readinmo(twoe, self.twoemofile)
-            return twoe
-        else: 
-            sys.exit("File '{0:s}' doesn't exist, exiting...".format(self.twoemofile))
-
-    def get_occupations(self):
-        '''Get the natural orbitla occupation numbers from the section 21 of the 
-           gamess-us dictionary file.'''
-# initialize numpy array to zeros
-        occ = np.zeros(self.mos, dtype=float)
-        onelectron.dictionary.readreals(self.dictionary, occ, 21)
-        return occ
-
-def print_twoe(twoe, nbf):  
-    '''Print the two-electron values.'''
-    ij=0 
-    for i in xrange(nbf):
-        for j in xrange(i+1):
-            ij += 1
-            kl = 0
-            for k in xrange(nbf):
-                for l in xrange(k+1):
-                    kl += 1
-                    if ij >= kl:
-                        if abs(twoe[ijkl(i,j,k,l)]) > 1.0e-10: 
-                            print "{0:3d}{1:3d}{2:3d}{3:3d} {4:25.14f}".format(
-                                i, j, k, l, twoe[ijkl(i,j,k,l)]) 
 
 def get_coulomb_1rdm(occ, twoe):
     '''Calculate the exact coulomb energy and return the matrix already
@@ -254,26 +132,6 @@ def pade_ac3(a, b, arg):
     p      = np.poly1d(coeffs)
     return p(arg)**2/(1.0 + p(arg)**2)
 
-def factor(i,j,k,l):
-    '''Based on the orbitals indices return the factor that takes into account 
-       the index permutational symmetry.'''
-    if i == j and k == l and i == k:
-        fijkl = 1.0
-    elif i == j and k == l:
-        fijkl = 2.0
-    elif (i == k and j == l) or (i == j and i == k) or (j == k and j == l) or (i == j or k == l):
-        fijkl = 4.0
-    else: 
-        fijkl = 8.0
-    return fijkl 
-
-def ijkl(i,j,k,l):
-    '''Based on the four orbital indices i,j,k,l return the address 
-       in the 1d vector.'''
-    ij = max(i, j)*(max(i, j) + 1)/2 + min(i, j)
-    kl = max(k, l)*(max(k, l) + 1)/2 + min(k, l)
-    return max(ij, kl)*(max(ij, kl) + 1)/2 + min(ij, kl)
-
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("logfile",
@@ -287,13 +145,13 @@ def main():
 #    mplot.plot(x, y1)
 #    mplot.show() 
 
-    gamess = Gamess(args.logfile)
+    gamess = gamessus.GamessParser(args.logfile)
 
-    twoemo = gamess.get_motwoe()
-    rdm2   = gamess.get_rdm2()
-    occ = gamess.get_occupations()
+    twoemo = gamess.read_twoemo()
+    rdm2   = gamess.read_rdm2()
+    occ = gamess.read_occupations()
 
-    nbf = gamess.mos   
+    nbf = gamess.get_number_of_mos()   
     
     a = 16.8212438522e00
     b = 2.61112559818e-04
