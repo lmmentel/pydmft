8534cf269773d4bb715dc9e947643412dfd8ebe4
Lukasz Mentel <lmmentel@gmail.com>
2014-02-16 05:36:20 +0100

    implemented PNOf4 properly as well as ELSPade (which is et not tested and added the possibility to print the curves together with one electron and nuclear repulsion energies from a data file

--- postcidmft.py
+++ postcidmft.py
@@ -2,15 +2,17 @@
 
 import docopt
 import numpy as np
-import matplotlib.pyplot as mplot
+from matplotlib import rc
+import matplotlib.pyplot as plt
 import os
 import re
 import sys
 from scipy.optimize import minimize
+from scipy.interpolate import interp1d
 from subprocess import Popen
 from gamessus import GamessReader, GamessParser
 from operator import itemgetter
-
+from collections import namedtuple
 
 class Functional(object):
     '''
@@ -47,6 +49,31 @@ class Functional(object):
 
         return np.sum(self.Jrdm) + np.sum(self.Krdm) + np.sum(self.Lrdm)
 
+    def J_energy(self):
+        '''
+        Calculate the J-term contribution to the electron-electron potential
+        energy corresponding to the functional.
+        '''
+
+        return np.sum(self.Jrdm)
+
+    def K_energy(self):
+        '''
+        Calculate the K-term contribution to the electron-electron potential
+        energy corresponding to the functional.
+        '''
+
+        return np.sum(self.Krdm)
+
+    def L_energy(self):
+        '''
+        Calculate the L-term contribution to the electron-electron potential
+        energy corresponding to the functional.
+        '''
+
+        return np.sum(self.Lrdm)
+
+
 class BB(Functional):
 
     def jkl_rdms(self, occ=None, Jints=None, Kints=None, **kwargs):
@@ -73,13 +100,42 @@ class PNOF4(Functional):
     as presented in M. Piris et al. JCP 133, 111101 (2010)
     '''
 
-    def jkl_rdms(self, occ=None, Jints=None, Kints=None):
+    def jkl_rdms(self, occ=None, Jints=None, Kints=None, homo=None):
 
         nbf = occ.size
         Jrdm = np.zeros((nbf, nbf), dtype=float)
         Krdm = np.zeros((nbf, nbf), dtype=float)
         Lrdm = np.zeros((nbf, nbf), dtype=float)
 
+        Ne = np.sum(occ[homo+1:])
+
+        for i in xrange(nbf):
+            ni = occ[i]
+            Jrdm[i, i] = 0.5*ni*Jints[i, i]
+            for j in xrange(i):
+                nj = occ[j]
+                if i <= homo and j <= homo:
+                    Jrdm[i, j] =  0.5*(ni*nj-(2.0-ni)*(2.0-nj))*Jints[i, j]
+                    Jrdm[j, i] = Jrdm[i, j]
+                    Krdm[i, j] = -0.25*(ni*nj-(2.0-ni)*(2.0-nj))*Kints[i, j]
+                    Krdm[j, i] = Krdm[i, j]
+                    Lrdm[i, j] = -0.5*np.sqrt((2.0-ni)*(2.0-nj))*Kints[i, j]
+                    Lrdm[j, i] = Lrdm[i, j]
+                elif i > homo and j > homo:
+                    Lrdm[i, j] = 0.5*np.sqrt(ni*nj)*Kints[i, j]
+                    Lrdm[j, i] = Lrdm[i, j]
+                elif i > homo and j <= homo:
+                    Jrdm[i, j] =  0.5*(ni*nj-((2.0-Ne)/Ne)*ni*(2.0-nj))*Jints[i, j]
+                    Jrdm[j, i] = Jrdm[i, j]
+                    Krdm[i, j] = -0.25*(ni*nj-((2.0-Ne)/Ne)*ni*(2.0-nj))*Kints[i, j]
+                    Krdm[j, i] = Krdm[i, j]
+                    t = (2.0-nj)*ni/Ne
+                    Lrdm[i, j] = -0.5*np.sqrt(t*(nj-ni+t))*Kints[i, j]
+                    Lrdm[j, i] = Lrdm[i, j]
+
+        self.Jrdm = Jrdm
+        self.Krdm = Krdm
+        self.Lrdm = Lrdm
 
 class DD(Functional):
     '''
@@ -200,28 +256,96 @@ class ELS2(Functional):
                 else:
                     Jrdm[i, j] = 0.5*ni*nj*Jints[i, j]
                     Jrdm[j, i] = Jrdm[i, j]
-                    Lrdm[i, j] = -0.25*ni*nj*Kints[i, j]
+                    Krdm[i, j] = -0.25*ni*nj*Kints[i, j]
+                    Krdm[j, i] = Krdm[i, j]
+
+        self.Jrdm = Jrdm
+        self.Krdm = Krdm
+        self.Lrdm = Lrdm
+
+class ELSPade(Functional):
+    '''
+    Object representing ELS DMFT functional that contains a parametrization in
+    terms of a rational Pade approximant,
+    see L. Mentel et al. "Consistent extension..." (2014)"
+    '''
+
+    def jkl_rdms(self, occ=None, Jints=None, Kints=None, homo=None, a=None, b=None):
+        '''
+        Calculate the energy matrix from the ELS2 functional.
+        '''
+
+
+        # if not provide use old AC3 parameters
+        if not a:
+            a = 16.8212438522           # AC3 a1
+        if not b:
+            b =  2.61112559818          # AC3 b1
+        #a = 19.569076362            # AC3 a2
+        #b =  1.41823370195          # AC3 b2
+
+        nbf = occ.size
+        Jrdm = np.zeros((nbf, nbf), dtype=float)
+        Krdm = np.zeros((nbf, nbf), dtype=float)
+        Lrdm = np.zeros((nbf, nbf), dtype=float)
+
+        for i in xrange(occ.size):
+            ni = occ[i]
+            Jrdm[i, i] = 0.5*ni*Jints[i, i]
+            for j in xrange(i):
+                nj = occ[j]
+                if i < homo and j < homo:
+                    Jrdm[i, j] = 0.5*ni*nj*Jints[i, j]
+                    Jrdm[j, i] = Jrdm[i, j]
+                    Krdm[i, j] = -0.25*ni*nj*Kints[i, j]
+                    Krdm[j, i] = Krdm[i, j]
+                elif j == homo and i > homo:
+                    Lrdm[i, j] = -0.5*np.sqrt(ni*nj)*Kints[i, j]
+                    Lrdm[j, i] = Lrdm[i, j]
+                elif j > homo and i > homo:
+                    Lrdm[i, j] = 0.5*np.sqrt(ni*nj)*Kints[i, j]
+                    Lrdm[j, i] = Lrdm[i, j]
+                elif j < homo and i > homo:
+                    pade = self.pade_ac3(a, b, 0.5*ni-0.5)
+                    Jrdm[i, j] = 0.5*ni*nj*Jints[i, j]
+                    Jrdm[j, i] = Jrdm[i, j]
+                    Krdm[i, j] = -0.25*ni*nj*pade*Kints[i, j]
+                    Krdm[j, i] = Krdm[i, j]
+                    Lrdm[i, j] = -0.5*np.sqrt(ni*nj)*(1-pade)*Kints[i, j]
                     Lrdm[j, i] = Lrdm[i, j]
 
         self.Jrdm = Jrdm
         self.Krdm = Krdm
         self.Lrdm = Lrdm
 
-def get_coulomb_1rdm(occ, twoe):
-    '''Calculate the exact coulomb energy and return the matrix already
-        multiplied by a factor of 0.5.'''
-    coulomb = np.zeros((occ.size, occ.size), dtype=float)
+    def pade_ac3(a, b, arg):
+        '''
+        Pade approximant used in the intrpolation scheme.
+        '''
 
-    for i in xrange(occ.size):
-        for j in xrange(occ.size):
-            coulomb[i, j] = occ[i]*occ[j]*twoe[ijkl(i,i,j,j)]
-
-    return 0.5*coulomb
+        coeffs = a*np.array([1.0, -4.0*b/3, -0.5, b, 0.0])
+        p      = np.poly1d(coeffs)
+        return p(arg)**2/(1.0 + p(arg)**2)
 
 class Exact(Functional):
     '''
     Exact functional based on the exact 2RDM
     '''
+
+    def Coulomb(self, occ=None, Jints=None):
+        '''
+        Calculate the exact coulomb energy and return the matrix already multiplied
+        by a factor of 0.5.
+        '''
+
+        coulomb = np.zeros((occ.size, occ.size), dtype=float)
+
+        for i in xrange(occ.size):
+            for j in xrange(occ.size):
+                coulomb[i, j] = occ[i]*occ[j]*Jints[i, j]
+
+        return 0.5*coulomb
+
 
     def jk_rdms(self, nbf=None, rdm2=None, Jints=None, Kints=None, homo=None):
         '''
@@ -311,14 +435,7 @@ def print_components(comps_dict):
     for key in sorted(comps_dict.iterkeys()):
         print "{0:<25s} : {1:>15.10f}".format(key, comps_dict[key])
 
-def qoly(poly, arg):
-    return poly(arg)**2/(1.0 + poly(arg)**2)
 
-def pade_ac3(a, b, arg):
-    
-    coeffs = a*np.array([1.0, -4.0*b/3, -0.5, b, 0.0])
-    p      = np.poly1d(coeffs)
-    return p(arg)**2/(1.0 + p(arg)**2)
 
 def factor(i,j,k,l):
     '''Based on the orbitals indices return the factor that takes into account 
@@ -329,12 +446,12 @@ def factor(i,j,k,l):
         fijkl = 2.0
     elif (i == k and j == l) or (i == j and i == k) or (j == k and j == l) or (i == j or k == l):
         fijkl = 4.0
-    else: 
+    else:
         fijkl = 8.0
-    return fijkl 
+    return fijkl
 
 def ijkl(i,j,k,l):
-    '''Based on the four orbital indices i,j,k,l return the address 
+    '''Based on the four orbital indices i,j,k,l return the address
        in the 1d vector.'''
     ij = max(i, j)*(max(i, j) + 1)/2 + min(i, j)
     kl = max(k, l)*(max(k, l) + 1)/2 + min(k, l)
@@ -369,7 +486,7 @@ def get_fci():
     return energies
 
 def get_error(x0, *args):
-    
+
     jobs = args[0]
     diffs = []
     for job in jobs:
@@ -384,7 +501,7 @@ def get_error(x0, *args):
         els    = get_else_matrix(nocc, twoeno, gamess.homo, x0[0], x0[1])
         dd     = decompose(els, gamess.homo)
         diffs.append(energy-dd["inner-outer"])
-        print "\n{1}\nELS for {0}\n{1}\n".format(file, "="*(len(file)+8)) 
+        print "\n{1}\nELS for {0}\n{1}\n".format(file, "="*(len(file)+8))
         print_components(dd)
     diffs = np.asarray(diffs)
     error = np.sqrt(np.add.reduce(diffs*diffs))
@@ -392,30 +509,24 @@ def get_error(x0, *args):
     print "-"*106
     return error
 
-def write_dmft_input(glog, dmftinput, functional=9, a1=0.0, b1=0.0):
-    '''Get system information from gamess log and write the dmft 
-       input file.''' 
-    input_dict = get_input_data(glog, functional, a1, b1) 
-    inp = open(dmftinput, 'w')
-    inp.write("&input\n")
-    for key in sorted(input_dict.iterkeys()):
-        inp.write("\t{0:s}={1:s}\n".format(key, str(input_dict[key])))
-    inp.write("/\n")
-    inp.close()
 
-def splitlist(l, n): 
+def splitlist(l, n):
+    '''
+    Split a list 'l' into lists of at most 'n' elements.
+    '''
+
     if len(l) % n == 0:
         splits = len(l)/n
     elif len(l) % n != 0 and len(l) > n:
         splits = len(l)/n+1
     else:
-        splits = 1 
+        splits = 1
 
     for i in xrange(splits):
         yield l[n*i:n*i+n]
 
 def get_error_scf(x0, *args):
-    
+
     executable = "/home/lmentel/Source/dmft/dmft_code/dmft.x"
     renergy = r'\s*Electron Interaction Energy\s*(\-?\d+\.\d+)'
     jobs = args[0]
@@ -433,7 +544,7 @@ def get_error_scf(x0, *args):
             p = Popen([executable, dmftinput], stdout=out, stderr=out)
             out.close()
             processes.append(p)
-    
+
         for p in processes: p.wait()
 
     for job in jobs:
@@ -468,44 +579,116 @@ def get_jkints(twoe, nb):
 
     return Jints, Kints
 
+def print_energies(fun_name, energies):
+
+
+    print "\n{0:^118s}\n{1:^118s}\n{0:^118s}\n".format("="*len(fun_name), fun_name)
+
+    print "{0:<30s}  {1:^6s}{2:^20s}{3:^20s}{4:^20s}{5:^20s}\n{6:s}".format("File",
+            "R", "J energy", "K energy", "L energy", "Total E_ee", "="*118)
+    for row in sorted(energies, key=itemgetter("dist")):
+        print "{0:<30s}  {1:6.2f}{2:20.10f}{3:20.10f}{4:20.10f}{5:20.10f}".format(row["file"],
+            row["dist"], row["j"], row["k"], row["l"], row["total"])
+    print
+
+def lplot(casesd, functs=[], save=False):
+
+    x, te, onee, twoe, nucr, hf = np.loadtxt(casesd['tablefile'], dtype=float, comments="#", usecols=(0,1,2,3,4,5), unpack=True)
+
+    te_s = interp1d(x, te, kind='cubic')
+    hf_s = interp1d(x, hf, kind='cubic')
+    xnew = np.linspace(min(x), max(x), 100)
+
+    rc('font', size=18.0)
+    rc('font',**{'family':'serif','serif':['Palatino']})
+    rc('text', usetex=True)
+    rc('figure', autolayout=True)
+
+    plt.figure(figsize=(12, 8))
+
+    plt.title(casesd['name'])
+    plt.xlabel(r'Internuclear distance $R$ [bohr]')
+    plt.ylabel(r'Energy [hartree]')
+    plt.plot(xnew, te_s(xnew), '-', linewidth='2.0', label="Exact")
+    plt.plot(xnew, hf_s(xnew), '-', label="HF")
+    for funct in functs:
+        for name, energies in funct.items():
+            fun_s = interp1d(x, onee+nucr+energies, kind='cubic')
+            plt.plot(xnew, fun_s(xnew), label=name)
+
+    plt.legend(loc="best", frameon=False)
+
+    if save:
+        figname = casesd['name']+".pdf"
+        plt.savefig(figname)
+    else:
+        plt.show()
+
 
 def main():
     '''
     Usage:
-        postcidmft.py <path>
+        postcidmft.py <molecule>
 
     Options:
-        path  path to the workdir
+        molecule  molecule to work with
     '''
 
-    args = docopt.docopt(main.__doc__, help=True)
+    workdir = '/home/lmentel/work/jobs/dmft'
+    cases = {'lih'  : {'name'     : r'LiH',
+                       'workdir'  : os.path.join(workdir, 'LiH'),
+                       'tablefile': os.path.join(workdir, 'LiH.tbl')},
+             'li2'  : {'name'     : r'Li$_2$',
+                       'workdir'  : os.path.join(workdir, 'Li2'),
+                       'tablefile': os.path.join(workdir, 'Li2.tbl')},
+             'behp' : {'name'     : r'BeH$^+$',
+                      'workdir'   : os.path.join(workdir, 'BeHp'),
+                      'tablefile' : os.path.join(workdir, 'BeHp.tbl')},
+            }
 
-    res = []
-    print "{0:<30s}   {1:^20s}{2:^20s}{3:^20s}{4:^20s}\n{5:s}".format("File", "CIDD", "ELS1", "ELS2", "Exact", "="*113)
-    for (path, dirs, files) in os.walk(args['<path>']):
+    args = docopt.docopt(main.__doc__, help=True)
+    if not args['<molecule>'] in cases.keys():
+        sys.exit('wrong molecule: {0:s}, should be one of (lih, behp, li2)')
+
+
+    functionals = [BB('BB'), PNOF4("PNOF4"), DD("DD"), ELS1("ELS1"), ELS2("ELS2")]
+
+    data = {}
+    for f in functionals:
+        data[f.name] = []
+    dist_re = re.compile(r'.*_(\d+\.\d+)_.*')
+
+    for (path, dirs, files) in os.walk(cases[args['<molecule>']]['workdir']):
         for fileitem in files:
-            if "_NO.log" in fileitem and "BeH" in fileitem:
+            if "_NO.log" in fileitem:
                 log = os.path.join(path, fileitem)
-                gp = GamessParser(log)
-                gr = GamessReader(log)
+                gp  = GamessParser(log)
+                gr  = GamessReader(log)
 
                 twoemo = gr.read_twoemo()
-                occ    = gr.read_occupations()
-
+                occ    = np.abs(gr.read_occupations())
                 Jints, Kints = get_jkints(twoemo, gp.get_number_of_mos())
 
-                functionals = [DD("DD"), ELS1("ELS1"), ELS2("ELS2")]
+                match = dist_re.search(fileitem)
                 for f in functionals:
                     f.jkl_rdms(occ=occ, Jints=Jints, Kints=Kints, homo=gp.get_homo())
-                res.append({"file"  : fileitem,
-                            "dd"    : functionals[0].total_energy(),
-                            "els1"  : functionals[1].total_energy(),
-                            "els2"  : functionals[2].total_energy(),
-                            "exact" : gp.get_ci_ee_energy()})
 
-    for row in sorted(res, key=itemgetter("file")):
-        print "{0:<30s}   {1:20.10f}{2:20.10f}{3:20.10f}{4:20.10f}".format(row["file"],
-            row["dd"], row["els1"], row["els2"], row["exact"])
+                for f in functionals:
+                    data[f.name].append({"file"  : fileitem,
+                                "dist"  : float(match.group(1)),
+                                "j"     : f.J_energy(),
+                                "k"     : f.K_energy(),
+                                "l"     : f.L_energy(),
+                                "total" : f.total_energy(),
+                               })
+    datap = []
+    for funct, values in data.items():
+        dde = [x["total"] for x in sorted(values, key=itemgetter("dist"))]
+        datap.append({funct : dde})
+        print_energies(funct, values)
+
+    lplot(cases[args['<molecule>']], datap, save=False)
+
 
 if __name__ == "__main__":
     main()
